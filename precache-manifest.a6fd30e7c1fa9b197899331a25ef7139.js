924d2692c7687ed22e7cd291cbdd0afea83c66bb
ilovepretzels <evg.shhh@gmail.com>
2019-05-05 00:07:55 +0300

    Updates

---
+++ precache-manifest.a6fd30e7c1fa9b197899331a25ef7139.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf90a9b39706827a08394807d0e9957",
+    "url": "/candlestick-charts/index.html"
+  },
+  {
+    "revision": "78e5088ff69b9d39a555",
+    "url": "/candlestick-charts/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "05b9d171541d0cdea5ad",
+    "url": "/candlestick-charts/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "78e5088ff69b9d39a555",
+    "url": "/candlestick-charts/static/js/2.5f75e29b.chunk.js"
+  },
+  {
+    "revision": "05b9d171541d0cdea5ad",
+    "url": "/candlestick-charts/static/js/main.a6328dbf.chunk.js"
+  },
+  {
+    "revision": "c0cfe53beb4e4e2ad2b6",
+    "url": "/candlestick-charts/static/js/runtime~main.63581e7b.js"
+  }
+]);
